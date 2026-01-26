da9ea46f45f2ba2173413bed319779b14e145b52
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2026-01-26 06:25:26 +0200

    add normalize_role function

---
+++ ingestion/test_adzuna.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+URL = "https://api.adzuna.com/v1/api/jobs/gb/search/1"
+
+PARAMS = {
+    "app_id": "dad4df96",
+    "app_key": "773246c9d5b50e31eaf8d66151785d04",
+    "results_per_page": 10,
+}
+
+response = requests.get(URL, params=PARAMS)
+
+data = response.json()
+
+
+for job in data['results']:
+    print(job)
+
